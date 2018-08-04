be4567f006ef1e3a871c987ae315a14d7ba81e3c
Scott McMullan <scott.mcmullan@griffithuni.edu.au>
2018-08-04 21:59:19 +1000

    AWR

--- app/src/main/java/com/ymir/fern_app/Person.java
+++ app/src/main/java/com/ymir/fern_app/Person.java
@@ -2,25 +2,88 @@ package com.ymir.fern_app;
 
 import java.util.ArrayList;
 
-public class Person extends SuperFern {
-    private float distance;
-    private ArrayList<String> attending = new ArrayList<>();
+public class Person {
+    private int personId;
+    private String firstName;
+    private String secondName;
+    private String university;
+    private String campus;
+    private String degree;
+    private String bio;
+    private double longitude;
+    private double latitude;
 
-    public float getDistance()
-    {
-        return this.distance;
+    
+
+    public int getPersonId() {
+        return personId;
     }
 
-    public void setDistance(float distance)
-    {
-        this.distance = distance;
+    public void setPersonId(int personId) {
+        this.personId = personId;
     }
 
-    public ArrayList<String> getAttending() {
-        return this.attending;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setAttending(ArrayList<String> attending) {
-        this.attending = attending;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
     }
 }
