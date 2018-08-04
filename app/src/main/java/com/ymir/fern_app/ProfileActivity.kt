2b2c84fd7b334204ffcea0642951d1223f3c3bb9
sk0g <ani.mehta@mail.com>
2018-08-05 09:02:23 +1000

    Added button to action bar in profile activity

--- app/src/main/java/com/ymir/fern_app/ProfileActivity.kt
+++ app/src/main/java/com/ymir/fern_app/ProfileActivity.kt
@@ -2,12 +2,17 @@ package com.ymir.fern_app
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import com.ymir.fern_app.ActivitySwitcher.switchToEvents
 import com.ymir.fern_app.ActivitySwitcher.switchToMap
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var mMapButton: Button
+    private lateinit var mEventButton: Button
+    private lateinit var mEditProfileButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -16,6 +21,32 @@ class ProfileActivity : AppCompatActivity() {
 
         mMapButton = findViewById(R.id.activity_profile_maps_button)
         mMapButton.setOnClickListener {view ->
-            switchToMap(applicationContext, view) }
+            switchToMap(applicationContext, view)
+        }
+
+        mEventButton = findViewById(R.id.activity_profile_events_button)
+        mEventButton.setOnClickListener {view ->
+            switchToEvents(applicationContext, view)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_bar, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        var id = item?.itemId
+
+        if (id == R.id.activity_profile_edit_text) {
+            editProfile()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun editProfile() {
+        return
     }
 }
