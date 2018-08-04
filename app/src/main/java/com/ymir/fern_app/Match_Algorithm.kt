81f0c1dcceb5a36e7f1dba51b03d112cc1f7a86b
sk0g <ani.mehta@mail.com>
2018-08-04 23:46:42 +1000

    Added Haversine formula for calculating distance between points

--- app/src/main/java/com/ymir/fern_app/Match_Algorithm.kt
+++ app/src/main/java/com/ymir/fern_app/Match_Algorithm.kt
@@ -1,2 +1,21 @@
 package com.ymir.fern_app
 
+class MatchingAlgorithm {
+
+    fun Haversine(lat1: Double, long1: Double, lat2: Double, long2: Double): Double {
+        // Returns the distance between two co-ordinates,
+        // given individual latitudes and longitudes
+
+        val approximateEarthRadius = 6371
+
+        val latDistance  = Math.toRadians(lat2 - lat1)
+        val longDistance = Math.toRadians(long2 - long1)
+
+        val l1 = Math.toRadians(lat1)
+        val l2 = Math.toRadians(lat2)
+
+        val a: Double = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.sin(longDistance / 2) * Math.sin(longDistance / 2) * Math.cos(l1) * Math.cos(l2)
+        val c: Double = 2 * Math.asin(Math.sqrt(a))
+        return approximateEarthRadius * c
+    }
+}
