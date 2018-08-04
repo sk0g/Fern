e7fff34c0d5df522cbd2abf7b62d75d4285aabca
Scott McMullan <scott.mcmullan@griffithuni.edu.au>
2018-08-04 22:14:48 +1000

    sefsadfa

--- app/src/main/java/com/ymir/fern_app/ProfileActivity.java
+++ app/src/main/java/com/ymir/fern_app/ProfileActivity.java
@@ -1,14 +1,29 @@
 package com.ymir.fern_app;
 
+import java.util.ArrayList;
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Bundle;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private Button mMapButton;
+    private Connection con;
+    private String dbUsername, dbPassword, dbIP, dbName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +42,33 @@ public class ProfileActivity extends AppCompatActivity {
     private void switchToMaps() {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+    }
+
+    @SuppressLint("NewApi")
+    public Connection connectionclass(String user, String password, String database, String server)
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://" + server + database + ";user=" + user+ ";password=" + password + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (SQLException se)
+        {
+            Log.e("error here 1 : ", se.getMessage());
+        }
+        catch (ClassNotFoundException e)
+        {
+            Log.e("error here 2 : ", e.getMessage());
+        }
+        catch (Exception e)
+        {
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        return connection;
     }
 }
