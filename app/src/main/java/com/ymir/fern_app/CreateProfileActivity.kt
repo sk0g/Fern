0ee073b048caf7e721fa841a4b6b2a937a1b9a62
sk0g <ani.mehta@mail.com>
2018-08-05 02:17:03 +1000

    Adjust order of operations to minimise variables being passed

--- app/src/main/java/com/ymir/fern_app/CreateProfileActivity.kt
+++ app/src/main/java/com/ymir/fern_app/CreateProfileActivity.kt
@@ -2,7 +2,6 @@ package com.ymir.fern_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import com.ymir.fern_app.ActivitySwitcher.switchToProfile
@@ -28,14 +27,14 @@ class CreateProfileActivity : AppCompatActivity() {
         mDegree = findViewById(R.id.create_profile_degree)
         mBio = findViewById(R.id.create_profile_bio)
 
-
         mSubmitButton = findViewById(R.id.create_profile_submit_button)
         mSubmitButton.setOnClickListener { view ->
-            submitData(view)
+            submitData()
+            switchToProfile(applicationContext, view)
         }
     }
 
-    fun submitData(view: View) {
+    fun submitData() {
         val person: Person = Person.getInstance()
 
         person.firstName = mFirstName.text.toString()
@@ -44,7 +43,5 @@ class CreateProfileActivity : AppCompatActivity() {
         person.campus = mCampus.text.toString()
         person.degree = mDegree.text.toString()
         person.bio = mBio.text.toString()
-
-        switchToProfile(applicationContext, view)
     }
 }
